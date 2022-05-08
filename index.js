ef738f21505824000a9db53cbdfd4a8a1d9b6901
abdulawalemon1 <emondx4@gmail.com>
2022-05-08 17:37:45 +0600

    My item delete method added

--- index.js
+++ index.js
@@ -86,6 +86,13 @@ async function run() {
             const result = await productCollection.insertOne(item)
             res.send(result);
         })
+
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
